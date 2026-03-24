cf7373c4b106b8e992ba4ef67a2386184679f581
Shubham Awasthi <shubhamawasthi021@gmail.com>
2026-03-24 22:37:06 +0530

    publush

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,7 +6,7 @@ import { Signup } from './pages/Signup'
 import { Signin } from './pages/Signin'
 import { Blog } from './pages/Blog'
 import { Blogs } from './pages/Blogs'
-import { Publish } from './pages/publish'
+import { Publish } from './pages/Publish'
 function App() {
   
 
